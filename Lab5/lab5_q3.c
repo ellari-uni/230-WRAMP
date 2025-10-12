fd284bf1e0396aeb7231669328af2851e9ce606b
Gillanders, Hayden (Student) <hg234@students.waikato.ac.nz>
2025-10-13 09:34:14 +1300

    Add comments

--- Lab5/lab5_q3.c
+++ Lab5/lab5_q3.c
@@ -8,16 +8,20 @@
 void count(int start, int end){
     int i;
 
+    // If start and end are the same, just write it once and return
     if (start == end){
         writessd(start);
         delay();
         return;
     }
+
+    // Else if start is less than end, count from start to end
     if (start < end){
         for (i = start; i <= end; i++){
             writessd(i);
             delay();
         }
+    // else (Start is greater than end), count from end to start
     } else {
         for (i = start; i >= end; i--){
             writessd(i);
--- Lab5/lab5_q4.s
+++ Lab5/lab5_q4.s
@@ -1,3 +1,8 @@
+# Main Entry point for this program
+# Reads the switch values from the board
+# The lower 8 bits are the start value, the upper 8 bits are the end value
+# Calls the count coroutine to output count progress to the seven segment display
+
 .text
 .global main
 
@@ -6,10 +11,11 @@ main:
     andi $8, $1, 0xFF # mask to get the lower 8 bits
     srli $9, $1, 8    # shift right to get the upper
 
-    subui $sp, $sp, 2
+    subui $sp, $sp, 2 # Create room on stack for variables (2 words)
+
     sw $9, 1($sp)   # store upper 8 bits (end)
     sw $8, 0($sp)   # store lower 8 bits (start)
 
     jal count
 
-    addui $sp, $sp, 2
+    addui $sp, $sp, 2 # Revert stack modification
